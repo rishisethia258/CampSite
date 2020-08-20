fcd14fa606738d0e0917d7e0641b38cc70c3ea9f
rishisethia <rishisethia258@gmail.com>
2020-08-20 14:12:24 +0530

    Add show.ejs page and make it functional

--- app.js
+++ app.js
@@ -15,7 +15,8 @@ app.set("view engine","ejs");
 
 var campgroundSchema = new mongoose.Schema({
 	name : String,
-	image : String
+	image : String,
+	description : String
 });
 
 var Campground = mongoose.model("Campground",campgroundSchema);
@@ -23,7 +24,8 @@ var Campground = mongoose.model("Campground",campgroundSchema);
 // Campground.create(
 // 	{
 // 		name: "Granite Hill",
-// 		image: "https://images.unsplash.com/photo-1504280390367-361c6d9f38f4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+// 		image: "https://images.unsplash.com/photo-1504280390367-361c6d9f38f4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+// 		description: "This is a huge granite hill, no bathrooms. No water. Beautiful Granite!"
 // 	}, 
 // 	function(err,campground) {
 // 		if(err)
@@ -41,20 +43,23 @@ app.get("/",function(req,res){
 	res.render("landing");
 });
 
+// INDEX - Show all campgrounds
 app.get("/campgrounds",function(req,res){
 	Campground.find({},function(err,allCampgrounds){
 		if(err)
 			console.log(err);
 		else
-			res.render("campgrounds",{campgrounds: allCampgrounds});		
+			res.render("index",{campgrounds: allCampgrounds});		
 	});
 	
 });
 
+// CREATE - Add new campground to Database
 app.post("/campgrounds",function(req,res){
 	var name = req.body.name;
 	var image = req.body.image;
-	var newCampground = {name:name ,image:image};
+	var description = req.body.description;
+	var newCampground = {name:name ,image:image ,description: description};
 	Campground.create(newCampground,function(err,newlyCreated){
 		if(err)
 			console.log(err);
@@ -63,9 +68,18 @@ app.post("/campgrounds",function(req,res){
 	});
 });
 
+// NEW - Show form to create new campground
 app.get("/campgrounds/new",function(req,res){
 	res.render("new");
 });
+
+// SHOW - Show more info about one campground
+app.get("/campgrounds/:id" ,function(req,res) {
+	Campground.findById(req.params.id,function(err,foundCampgound){
+		if(err) console.log(err);
+		else res.render("show",{campground: foundCampgound});
+	});
+})
 
 app.listen(3000,function(){
 	console.log("The CampSite Server Started!");
