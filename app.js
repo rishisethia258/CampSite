0695c58ec539eea482cadd837cbd71bd8b1f8fff
rishisethia <rishisethia258@gmail.com>
2020-08-21 15:53:59 +0530

    Add Create comments feature and improve views folder structure

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var app = express();
 var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
 var Campground = require("./models/campground");
+var Comment = require("./models/comment");
 var seedDB = require("./seeds");
 
 mongoose.connect('mongodb://localhost:27017/camp_site', {
@@ -27,7 +28,7 @@ app.get("/campgrounds",function(req,res){
 		if(err)
 			console.log(err);
 		else
-			res.render("index",{campgrounds: allCampgrounds});		
+			res.render("campgrounds/index",{campgrounds: allCampgrounds});		
 	});
 	
 });
@@ -48,16 +49,59 @@ app.post("/campgrounds",function(req,res){
 
 // NEW - Show form to create new campground
 app.get("/campgrounds/new",function(req,res){
-	res.render("new");
+	res.render("campgrounds/new");
 });
 
 // SHOW - Show more info about one campground
 app.get("/campgrounds/:id" ,function(req,res) {
 	Campground.findById(req.params.id).populate("comments").exec(function(err,foundCampgound){
 		if(err) console.log(err);
-		else res.render("show",{campground: foundCampgound});
+		else res.render("campgrounds/show",{campground: foundCampgound});
 	});
-})
+});
+
+// =================
+// COMMENT ROUTES
+// =================
+
+app.get("/campgrounds/:id/comments/new",function(req,res){
+	Campground.findById(req.params.id,function(err,campground){
+		if(err)
+		{
+			console.log(err);
+		}
+		else
+		{
+			res.render("comments/new",{campground : campground});		
+		}
+	});
+	
+});
+
+app.post("/campgrounds/:id/comments",function(req,res){
+	Campground.findById(req.params.id,function(err,campground){
+		if(err)
+		{
+			console.log(err);
+			res.redirect("/campground");
+		}
+		else
+		{
+			Comment.create(req.body.comment,function(err,comment){
+				if(err)
+				{
+					console.log(err);
+				}
+				else
+				{
+					campground.comments.push(comment);
+					campground.save();
+					res.redirect("/campgrounds/" + campground._id);
+				}
+			});
+		}
+	});
+});
 
 app.listen(3000,function(){
 	console.log("The CampSite Server Started!");
