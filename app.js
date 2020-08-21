147ae1fac69e02db11729a34dad02ee2f8d20dc3
rishisethia <rishisethia258@gmail.com>
2020-08-21 19:39:10 +0530

    Style show page

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ mongoose.connect('mongodb://localhost:27017/camp_site', {
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.set("view engine","ejs");
+app.use(express.static(__dirname + "/public"));
 
 seedDB();
 
