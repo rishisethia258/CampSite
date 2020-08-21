c194901a58385cbf6577cecf974ad71f4b85f639
rishisethia <rishisethia258@gmail.com>
2020-08-21 14:05:06 +0530

    Add comments in database

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
+var Campground = require("./models/campground");
+var seedDB = require("./seeds");
 
 mongoose.connect('mongodb://localhost:27017/camp_site', {
   useNewUrlParser: true,
@@ -13,31 +15,7 @@ mongoose.connect('mongodb://localhost:27017/camp_site', {
 app.use(bodyParser.urlencoded({extended:true}));
 app.set("view engine","ejs");
 
-var campgroundSchema = new mongoose.Schema({
-	name : String,
-	image : String,
-	description : String
-});
-
-var Campground = mongoose.model("Campground",campgroundSchema);
-
-// Campground.create(
-// 	{
-// 		name: "Granite Hill",
-// 		image: "https://images.unsplash.com/photo-1504280390367-361c6d9f38f4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
-// 		description: "This is a huge granite hill, no bathrooms. No water. Beautiful Granite!"
-// 	}, 
-// 	function(err,campground) {
-// 		if(err)
-// 		{
-// 			console.log(err);
-// 		}
-// 		else
-// 		{
-// 			console.log("NEWLY CREATED CAMPGROUND");
-// 			console.log(campground);
-// 		}
-// 	});
+seedDB();
 
 app.get("/",function(req,res){
 	res.render("landing");
@@ -75,7 +53,7 @@ app.get("/campgrounds/new",function(req,res){
 
 // SHOW - Show more info about one campground
 app.get("/campgrounds/:id" ,function(req,res) {
-	Campground.findById(req.params.id,function(err,foundCampgound){
+	Campground.findById(req.params.id).populate("comments").exec(function(err,foundCampgound){
 		if(err) console.log(err);
 		else res.render("show",{campground: foundCampgound});
 	});
